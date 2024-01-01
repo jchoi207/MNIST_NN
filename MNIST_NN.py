2ded73cdc1b2130180a98f04b3c6a1f61af92e9e
jchoi207 <72828126+jchoi207@users.noreply.github.com>
2024-01-01 17:43:05 -0500

    Update MNIST_NN.py

--- MNIST_NN.py
+++ MNIST_NN.py
@@ -21,7 +21,7 @@ Y_train = data_train[0]
 X_train = data_train[1:n] 
 X_train = X_train / 255.
 
-def init_params(): 
+def initialization(): 
     b1 = np.random.rand(10, 1) - 0.5 
     b2 = np.random.rand(10, 1) - 0.5
     W1 = np.random.rand(10, 784) - 0.5
@@ -50,7 +50,7 @@ def one_hot(Y):
 def deriv_ReLU(Z):
     return Z > 0 # boolean converted to numbers, 
     
-def backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y):
+def back_prop(Z1, A1, Z2, A2, W1, W2, X, Y):
     one_hot_Y = one_hot(Y)
     dZ2 = A2 - one_hot_Y
     dW2 = 1 / m * dZ2.dot(A1.T)
@@ -61,7 +61,7 @@ def backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y):
     return dW1, db1, dW2, db2
 
 
-def update_params(W1, b1, W2, b2,dW1, db1, dW2, db2, alpha):
+def update(W1, b1, W2, b2,dW1, db1, dW2, db2, alpha):
     W1 = W1 - alpha * dW1 
     b1 = b1 - alpha * db1 
     W2 = W2 - alpha * dW2 
@@ -72,23 +72,23 @@ def update_params(W1, b1, W2, b2,dW1, db1, dW2, db2, alpha):
 def get_predictions(A2):
     return np.argmax(A2, 0)
 
-def get_accuracy(predictions, Y):
+def accuracy(predictions, Y):
     print(predictions, Y)
     return np.sum(predictions == Y) / Y.size
 
 def gradient_descent(X, Y, alpha, iterations):
-    W1, b1, W2, b2 = init_params()
+    W1, b1, W2, b2 = initialization()
     l = []
     for i in range(iterations):
         Z1, A1, Z2, A2 = forward_prop(W1, b1, W2, b2, X)
         dW1, db1, dW2, db2 = back_prop(Z1, A1, Z2, A2, W1, W2, X, Y)
-        W1, b1, W2, b2 = update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha)
+        W1, b1, W2, b2 = update(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha)
         if i % 10 == 0:
             print("Iteration: ", i)
             predictions = get_predictions(A2)
-            accuracy = get_accuracy(predictions, Y)
+            accuracy = accuracy(predictions, Y)
             l.append(accuracy)
-            print(get_accuracy(predictions, Y))
+            print(accuracy(predictions, Y))
             
     return W1, b1, W2, b2, l
 
